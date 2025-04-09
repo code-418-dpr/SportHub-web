ac87c8f600895972f845071e8b1d2d324a5cff83
Scorpi-ON <s.scorpi-on@ya.ru>
2025-04-09 21:21:52 +0300

    chore: minor changes

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,10 +1,13 @@
 import Link from "next/link";
 
-import { OauthButtons } from "@/components/auth/oauth-buttons";
+import { OAuthButtons } from "@/components/auth/oauth-buttons";
 import { LoginForm } from "@/components/forms/login-form";
 import { Button } from "@/components/ui/button";
 
-export default function LoginPage({ searchParams }: { searchParams: Record<string, string | string[] | undefined> }) {
+export default async function LoginPage(props: {
+    searchParams: Promise<Record<string, string | string[] | undefined>>;
+}) {
+    const searchParams = await props.searchParams;
     const registerUrl = `/register${
         Object.keys(searchParams).length > 0
             ? `?${new URLSearchParams(searchParams as Record<string, string>).toString()}`
@@ -15,7 +18,7 @@ export default function LoginPage({ searchParams }: { searchParams: Record<strin
         <div className="w-[min(calc(100%-2rem),400px)] space-y-4">
             <h1 className="pb-4 text-center text-3xl font-semibold">Добро пожаловать</h1>
             <LoginForm />
-            <OauthButtons />
+            <OAuthButtons />
             <Button variant="link" className="w-full font-normal" size="sm" asChild>
                 <Link href={registerUrl}>Ещё нет аккаунта?</Link>
             </Button>
--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,14 +1,15 @@
+import React from "react";
+
 import Link from "next/link";
 
-import { OauthButtons } from "@/components/auth/oauth-buttons";
+import { OAuthButtons } from "@/components/auth/oauth-buttons";
 import { RegisterForm } from "@/components/forms/register-form";
 import { Button } from "@/components/ui/button";
 
-export default function RegisterPage({
-    searchParams,
-}: {
-    searchParams: Record<string, string | string[] | undefined>;
-}): React.ReactNode {
+export default async function RegisterPage(props: {
+    searchParams: Promise<Record<string, string | string[] | undefined>>;
+}): Promise<React.ReactNode> {
+    const searchParams = await props.searchParams;
     const loginUrl = `/login${
         Object.keys(searchParams).length > 0
             ? `?${new URLSearchParams(searchParams as Record<string, string>).toString()}`
@@ -19,7 +20,7 @@ export default function RegisterPage({
         <div className="w-[min(calc(100%-2rem),400px)] space-y-4">
             <h1 className="pb-4 text-center text-3xl font-semibold">Создать аккаунт</h1>
             <RegisterForm />
-            <OauthButtons />
+            <OAuthButtons />
             <Button variant="link" className="w-full font-normal" size="sm" asChild>
                 <Link href={loginUrl}>Уже есть аккаунт?</Link>
             </Button>
