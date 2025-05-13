aeaf7497a773fa37081f031f54867a74cdbf341f
Scorpi-ON <s.scorpi-on@ya.ru>
2025-05-13 20:48:36 +0300

    fix: replaced bcrypt with bcryptjs

--- src/lib/auth-options.ts
+++ src/lib/auth-options.ts
@@ -1,4 +1,4 @@
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 
 import { NextAuthOptions, User } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
